9e531c1d6f11551f0e011372f8b5b687dc2dcfa1
Heet Doshi <doshiheet2872003@gmail.com>
2024-03-06 14:10:49 +0530

    home done

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,20 +1,21 @@
 import { Component, Input, OnInit, Output } from '@angular/core';
 import { Product } from '../../../types';
 import {RatingModule} from 'primeng/rating'
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { EventEmitter } from '@angular/core';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [RatingModule, FormsModule],
+  imports: [RatingModule, FormsModule,PaginatorModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
 export class ProductComponent implements OnInit {
   @Input() product!: Product;
   @Output() productOutput : EventEmitter<Product> = new EventEmitter<Product>
+
 
   ngOnInit(): void {
     this.productOutput.emit(this.product)
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductService } from '../services/product.service';
 import { __param } from 'tslib';
 import { Product, Products } from '../../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, ProductComponent, CommonModule],
+  imports: [RouterOutlet, ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -18,12 +19,24 @@ export class HomeComponent implements OnInit{
   constructor(private productService : ProductService){}
 
   products : Product[] =[];
+  totalPage :number=0;
+  rows:number = 4;
 
+  onProductOutput(product :Product){
+    console.log(product, 'Output')
+  }
+  onPageChnage(event:any){
+      this.fetchProducts(event.page , event.rows)
+  }
+
+  fetchProducts(page:number , perPage : number){
+    this.productService.getProducts('http://localhost:3000/clothes',{page,perPage})
+    .subscribe((products : Products)=>{
+      this.products = products.items;
+      this.totalPage = products.total;
+    })
+  }
   ngOnInit(): void {
-      this.productService
-      .getProducts('http://localhost:3000/clothes',{page: 0, perPage:8})
-      .subscribe((products : Products)=>{
-          this.products = products.items;
-      })
+     this.fetchProducts(0,this.rows);
   }
 }
